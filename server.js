6307478361219c0d2410e3edd39d2aabd761c4cf
Oleg Meserenko <meserenko.oleg@gmail.com>
2022-04-19 18:38:10 +0300

    finished server part for diploma shop project

---
+++ server.js
@@ -0,0 +1,33 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDatabase from "./config/MongoDb.js";
+import productRoute from "./Routes/ProductRoutes.js";
+import { errorHandler, notFound } from "./Middleware/Errors.js";
+import userRouter from "./Routes/UserRoutes.js";
+import orderRouter from "./Routes/orderRoutes.js";
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+const corsMiddleware = require('./Middleware/CorsMiddleware.cjs')
+const app = express();
+
+dotenv.config();
+app.use(corsMiddleware)
+connectDatabase();
+app.use(express.json());
+
+// API
+app.use("/api/products", productRoute);
+app.use("/api/users", userRouter);
+app.use("/api/orders", orderRouter);
+app.get("/api/config/paypal", (req, res) => {
+  res.send(process.env.PAYPAL_CLIENT_ID);
+});
+
+
+// ERROR HANDLER
+app.use(notFound);
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 1000;
+
+app.listen(PORT, console.log(`server run in port ${PORT}`));
